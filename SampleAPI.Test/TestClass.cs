da827b083344ba0f76dc4b913623712fda3e2d1d
Pravesh Chhabra <contact2pravesh@gmail.com>
2020-02-12 10:27:25 +0530

    Add Unit Test

---
+++ SampleAPI.Test/TestClass.cs
@@ -0,0 +1,23 @@
+﻿using SampleAPI.Controllers;
+using System;
+using Xunit;
+
+namespace SampleAPI.Test
+{
+    public class TestClass
+    {
+        private ValuesController _valuesController;
+
+        public TestClass()
+        {
+            _valuesController = new ValuesController();
+        }
+
+        [Fact]
+        public void Test_Get()
+        {
+            var result = _valuesController.Get();
+            Assert.Equal("value1", ((string[])result.Value)[0]);
+        }
+    }
+}
